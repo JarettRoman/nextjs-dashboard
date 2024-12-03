c33c76d7efb3550a0d45e26188380364bc2db995
JarettRoman <afrocious@gmail.com>
2024-12-03 12:29:15 -0500

    create invoice server action

---
+++ app/lib/actions.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { z } from 'zod';
+import { db } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+	id: z.string(),
+	customerId: z.string(),
+	amount: z.coerce.number(),
+	status: z.enum(['paid', 'pending']),
+	date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+	// const rawFormData = Object.fromEntries(formData.entries());
+	const { customerId, amount, status } = CreateInvoice.parse(Object.fromEntries(formData.entries()));
+	const amountInCents = amount * 100;
+	const date = new Date().toISOString().split('T')[0];
+
+	const client = await db.connect();
+
+	await client.sql`
+		INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+	revalidatePath('/dashboard/invoices');
+	redirect('/dashboard/invoices');
+}
